4f96e85a8f11c6e99e14b0fed9d02709f3bce2fc
Michael Blatherwick <michael@michaelblatherwick.co.uk>
2025-03-22 18:35:39 +0000

    Hide remaining and error letters until needed

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ function setRemaining() {
   }, [...source].join(''))
   var remainingSpans = remaining.split('').map(x => '<span>' + x + '</span>').join('')
   document.getElementById("remaining").innerHTML = remainingSpans
+  var remainingDiv = document.getElementById("remainingDiv")
+  if (remaining.length === 0) {
+    remainingDiv.style.display = 'none'
+  }
+  else remainingDiv.style.display = 'block'
 
   var errors = source.reduce((running, current) => {
     return running.replace(current, '')
   }, [...solution].join(''))
   var errorsSpans = errors.split('').map(x => '<span>' + x + '</span>').join('')
   document.getElementById("errors").innerHTML = errorsSpans
+  if (errors.length === 0) {
+    errorsDiv.style.display = 'none'
+  }
+  else errorsDiv.style.display = 'block'
 };
 
 document.getElementById("source")?.addEventListener("input", e => setRemaining())
